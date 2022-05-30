cbd0654a6f97a7474161213dcd29e28649d17e14
Axeldev <axl.bertrand.webdev@gmail.com>
2022-05-30 23:55:19 +0200

    route par défaut (début)
    vidéo 4 (5min)

---
+++ app/controllers/postsController.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Controllers\PostsController;
+
+function indexAction(\PDO $connection){
+    GLOBAL $content, $title;
+    $content = "<h1>TEST </h1>";
+}
--- app/router.php
+++ app/router.php
@@ -1,3 +1,10 @@
 <?php
 /* ROUTEUR PRINCIPAL */
 
+// ROUTE PAR DEFAUT
+//PATTERN: /
+// CTRL: postController
+//ACTION: index
+
+include_once '../app/controllers/postsController.php';
+\App\Controllers\PostsController\indexAction($connection);
