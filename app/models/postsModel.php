8076ad29fd7f6573f01be59185e34d76b794b115
AxouDev <axl.bertrand.webdev@gmail.com>
2022-06-09 14:47:46 +0200

    Liste des post récents
    Intégration de la liste des posts récents dans le aside

--- app/models/postsModel.php
+++ app/models/postsModel.php
@@ -34,3 +34,23 @@ function findOneById(\PDO $connection, $id) :array{
     $rs->execute();
     return $rs->fetch(\PDO::FETCH_ASSOC);
 }
+
+/**
+ * Undocumented function
+ *
+ * @param \PDO $connection
+ * @return array
+ */
+function findRecent(\PDO $connection) :array{
+    $sql = "SELECT COUNT(c.id) as nb_comment, p.title as post_title, p.created_at as post_date, p.image as post_image,
+                   a.firstname as author_firstname, a.lastname as author_lastname
+    FROM comments c
+    RIGHT JOIN posts p ON p.id = c.post_id
+    RIGHT JOIN authors a ON p.author_id = a.id
+    GROUP BY p.id
+    ORDER BY p.created_at DESC
+    LIMIT 3;  ";
+    $rs = $connection->prepare($sql);
+    $rs->execute();
+    return $rs->fetchAll(\PDO::FETCH_ASSOC);
+}
---
+++ app/views/posts/_recent.php
@@ -0,0 +1,24 @@
+<div class="sidebar-box ftco-animate">
+    <h3>Recent Blog</h3>
+    <?php 
+        include_once '../app/models/PostsModel.php';
+        $recentPosts = \App\Models\PostsModel\findRecent($connection);
+    ?>
+
+    <?php 
+        foreach($recentPosts as $post): 
+        $created_at = strtotime($post['post_date']);
+    ?>
+        <div class="block-21 mb-4 d-flex">
+            <a class="blog-img mr-4" style="background-image: url(images/<?php echo $post['post_image'] ?>);"></a>
+            <div class="text">
+                <h3 class="heading"><a href="#"><?php $post['post_title'] ?></a></h3>
+                <div class="meta">
+                    <div><a href="#"><span class="icon-calendar"></span><?php echo date('F d, Y', $created_at); ?></a></div>
+                    <div><a href="#"><span class="icon-person"></span><?php echo $post['author_firstname'] ?> <?php echo $post['author_lastname'] ?></a></div>
+                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                </div>
+            </div>
+        </div>
+    <?php endforeach;  ?>
+</div>
